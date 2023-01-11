ab1831ef79328e17a734bb6dac72e82ac6cf0e63
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-01-12 00:28:28 +0200

    3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  //   submitBtn: document.querySelector('.feedback-form  button'),
+};
+const USER = 'feedback-form-state';
+let formData = {
+  email: '',
+  message: '',
+};
 
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', onSubmit);
+function onTextareaInput(evt) {
+  formData.email = refs.form.email.value;
+  formData.message = refs.form.message.value;
+
+  //   formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(USER, JSON.stringify(formData));
+  //   console.log(evt.target);
+  //   console.log(JSON.parse(USER_DATA));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  if (!refs.form.email.value || !refs.form.message.value) {
+    alert('Не всі поля заповнені!');
+    return;
+  }
+  evt.currentTarget.reset();
+  console.log(formData);
+  localStorage.removeItem(USER);
+  formData = {
+    email: ' ',
+    message: ' ',
+  };
+}
+
+populateTextarea();
+function populateTextarea() {
+  const USER_DATA = localStorage.getItem(USER);
+  const b = JSON.parse(USER_DATA);
+
+  if (b) {
+    refs.form.elements.email.value = b.email;
+    refs.form.elements.message.value = b.message;
+  }
+  console.log(b);
+}
+// populateTextarea();
+//   refs.form.elements.email.value = 'b.email';
+//   refs.form.elements.message.value = 'b.message';
+//   //   console.log(refs.form.message.value);
+// }
+
+// refs.submitBtn.addEventListener('submit', onFormSubmit);
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   console.log(USER);
+//   localStorage.removeItem(USER);
+//   evt.currentTarget.reset();
+// }
+
+// if ( b.email && !b.message || b.message && !b.email)
